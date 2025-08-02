833f7563f099bb84626f1d188589a70de7a805f4
CodifyWithShravan <shravankumarthudi777@gmail.com>
2025-08-02 12:22:28 +0530

    Initial commit

--- Add3.java
+++
@@ -1,7 +0,0 @@
-public class Add3 {
-    public static void main(String[] args) {
-        int n1 = 3, n2 = 4, n3 = 5, sum;//initialisation
-        sum = n1 + n2 + n3;
-        System.out.println("The sum of these numbers is " + sum);
-    }
-}
